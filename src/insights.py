df8b0eb6cfc0e5232562a1d305b4d5a36be70a1e
GustavoAns <gugasato9@live.com>
2022-05-31 18:57:05 -0300

    Fix 3

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,15 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    all_jobs = read(path)
 
-    Must call `read`
+    all_industries = set()
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    for job in all_jobs:
+        if (job["industry"] != ""):
+            all_industries.add(job["industry"])
 
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return all_industries
 
 
 def filter_by_industry(jobs, industry):
