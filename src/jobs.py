f0f322d5c7f5e6d89891122223764a7cf4f1c83d
GustavoAns <gugasato9@live.com>
2022-05-31 16:21:04 -0300

    Creating jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,31 @@
 from functools import lru_cache
+import csv
 
-# Vamo q Vamo
+
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path, "r") as file:
+        file_csv = csv.reader(file, delimiter=",", quotechar='"')
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+        acumulator = []
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+        for line in file_csv:
+            obj = {
+                "job_title": line[0],
+                "company": line[1],
+                "state": line[2],
+                "city": line[3],
+                "min_salary": line[4],
+                "max_salary": line[5],
+                "job_desc": line[6],
+                "industry": line[7],
+                "rating": line[8],
+                "date_posted": line[9],
+                "valid_until": line[10],
+                "job_type": line[11],
+                "id": line[12],
+            }
+            acumulator.append(obj)
+
+    acumulator.pop(0)
+    return acumulator
