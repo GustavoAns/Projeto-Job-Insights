291e50e0c2f7674da78a6736f51dfb796cdff354
GustavoAns <gugasato9@live.com>
2022-06-01 17:03:45 -0300

    Add req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    word_py = "Python"
+    word_js = "Javascript"
+
+    assert count_ocurrences(path, word_py) == 1639
+    assert count_ocurrences(path, word_js) == 122
