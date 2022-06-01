c8dc9c91806ee6267d32dbe4fa6e5c424d327c7f
GustavoAns <gugasato9@live.com>
2022-06-01 17:21:03 -0300

    Add req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "src/jobs.csv"
+    jobs = read_brazilian_file(path)
+
+    for job in jobs:
+        assert 'title' in job
+        assert 'titulo' not in job
+        assert 'salary' in job
+        assert 'salario' not in job
+        assert 'type' in job
+        assert 'tipo' not in job
